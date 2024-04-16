023e3a78c7e7850a6111e5668838d11b9eb2b4c2
M. Riziq Sirfatullah Alfarizi <115126841+RiziqAden@users.noreply.github.com>
2024-04-16 20:33:41 +0700

    Add files via upload

---
+++ KELOMPOK12/kasus3.py
@@ -0,0 +1,12 @@
+# kelompok 12
+# Anggota Kelompok
+# M. Riziq Sirfatullah Alfarizi - i.2211266
+# Muhamad  Noufal Falah - i.2210466
+# Muhammad  Risyad Fadilah - i.2210148
+
+# kasus 3
+
+input = int(input("Masukkan banyak pengulangan: "))
+
+for i in range(input, 0, -1):
+    print(f"Baris ke-{i}")
